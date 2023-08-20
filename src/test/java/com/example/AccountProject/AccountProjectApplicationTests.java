ac8a7ad6f123b7ddcffe63c783e7a1993f490b06
Kanggyeonggu <kku1031@naver.com>
2023-08-20 16:55:19 +0900

    init

---
+++ src/test/java/com/example/AccountProject/AccountProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccountProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
