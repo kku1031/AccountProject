32613b983b6e5815f44d2c792101cda5e15af4a4
Kanggyeonggu <kku1031@naver.com>
2023-09-05 01:27:22 +0900

    Test

--- src/main/java/com/example/AccountProject/config/JpaAuditingConfiguration.java
+++ src/main/java/com/example/AccountProject/config/JpaAuditingConfiguration.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class JpaAuditingConfiguration {
     //스프링 애플리케이션 클래스를 시작할때 같이 동작.(DB에 데이터 저장할때, 어노테이션 붙은 부분들 같이 동작)
+    d
 }
